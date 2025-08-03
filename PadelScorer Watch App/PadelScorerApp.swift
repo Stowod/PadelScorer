4ca962409c461a180c9c5a2452c00cb6279e01d3
Stowod <emilian.pp@gmail.com>
2025-08-03 13:35:38 +0200

    Initial Commit

---
+++ PadelScorer Watch App/PadelScorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PadelScorerApp.swift
+//  PadelScorer Watch App
+//
+//  Created by Emilian Popp on 03.08.25.
+//
+
+import SwiftUI
+
+@main
+struct PadelScorer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PadelScorer/PadelScorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PadelScorerApp.swift
+//  PadelScorer
+//
+//  Created by Emilian Popp on 03.08.25.
+//
+
+import SwiftUI
+
+@main
+struct PadelScorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
